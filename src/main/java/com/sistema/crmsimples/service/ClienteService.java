d3f5eb6e8ddbfcef9188ba2b0282e3087eca48e7
pabloluis88 <pabloleite@outlook.com>
2025-08-15 16:46:23 -0300

    Criação da Classe service com metodos de criacao, listar todos e listar por id

---
+++ src/main/java/com/sistema/crmsimples/service/ClienteService.java
@@ -0,0 +1,34 @@
+package com.sistema.crmsimples.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sistema.crmsimples.model.Clientes;
+import com.sistema.crmsimples.repository.ClienteRepository;
+
+import jakarta.transaction.Transactional;
+
+public class ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    public Clientes salvar(Clientes cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Transactional(rollbackOn= Exception.class)
+    public List<Clientes> listarTodos() {
+        return  clienteRepository.findAll();
+    }
+    
+    @Transactional(rollbackOn= Exception.class)
+    public Optional<Clientes> listarPorId(UUID id) {
+        return clienteRepository.findById(id);
+    }
+    
+
+}
